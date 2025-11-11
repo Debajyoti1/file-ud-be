beef9ee9318bfa1728d64722aef8ab318ea47313
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2025-11-11 13:20:29 +0530

    disable x powered by

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const routes = require("./src/routes/index.routes");
 const requestIdMiddleware = require("./src/middleware/requestid.middleware");
 
 const app = express();
+app.disable("x-powered-by");
 
 app.use(cors({
   origin: ["http://localhost:5173","https://file.debajyotidutta.com"],
@@ -35,7 +36,7 @@ app.set("trust proxy", true);
 app.use(requestIdMiddleware);
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json({limit: '100kb' }));
+app.use(express.json());
 app.use(cookieParser());
 
 // Routes
